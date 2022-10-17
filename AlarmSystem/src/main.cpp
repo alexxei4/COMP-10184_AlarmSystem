4ef7288f428a66380e2732d696378482e481f364
alexxei4 <51131925+alexxei4@users.noreply.github.com>
2022-10-17 11:32:44 -0400

    Update main.cpp

--- AlarmSystem/src/main.cpp
+++ AlarmSystem/src/main.cpp
@@ -1,6 +1,7 @@
 // COMP-10184 – Mohawk College 
-// PIR Sensor Test Application 
-// Turn on the blue LED with motion is detected. 
+// Alarm System
+// This is an alarm system assignment , when you wave your hand over the motion sensor , it perfroms certain light commands depending on which
+// stage you reached 
 
 /// I , Alexei Ougriniouk, Student number 000776331, certify this is my own work 
 /// STAGE REACHED = 3
@@ -11,9 +12,9 @@
 #define PIN_PIR D5 
 #define PIN_BUTTON D6 
 
-int  sevensecdelay = 7500;
+int  iSevenSecDelay = 7500;
 
-int quartersec = 250;
+int iQuarterSecDelay = 250;
 
  
  
@@ -44,25 +45,25 @@ void loop() {
   /// STAGE 2 : Light turns either on or off every quarter second 
   digitalWrite(LED_BUILTIN, bPIR); 
   // wait quarter second 
-  delay(quartersec); 
+  delay(iQuarterSecDelay); 
  
   // turn the LED OFF 
   digitalWrite(LED_BUILTIN, !bPIR); 
   // wait quarter second
-  delay(quartersec); 
+  delay(iQuarterSecDelay); 
 
   digitalWrite(LED_BUILTIN, bPIR); 
   // wait quarter second 
-  delay(quartersec); 
+  delay(iQuarterSecDelay); 
  
   // turn the LED OFF 
   digitalWrite(LED_BUILTIN, !bPIR); 
   // wait quarter second
-  delay(quartersec); 
+  delay(iQuarterSecDelay); 
   digitalWrite(LED_BUILTIN, bPIR); 
 
   ///this is stage 1  7500 ms = 7.5s , 7.5 + 2.5s = 10s 
-  delay(sevensecdelay);
+  delay(iSevenSecDelay);
   
  
   // send the PIR signal directly to the LED 
